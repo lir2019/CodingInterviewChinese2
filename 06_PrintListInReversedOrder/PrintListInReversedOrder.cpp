828c90b91165d0f64564db966fa19ca2ae3d8580
lirui2020 <abc15656931979@gmail.com>
2022-12-20 08:36:42 +0800

    06, 07

--- 06_PrintListInReversedOrder/PrintListInReversedOrder.cpp
+++ 06_PrintListInReversedOrder/PrintListInReversedOrder.cpp
@@ -15,8 +15,131 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 面试题6：从尾到头打印链表
 // 题目：输入一个链表的头结点，从尾到头反过来打印出每个结点的值。
 
-#include "..\Utilities\List.h"
 #include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode
+{
+    int       m_nValue;
+    ListNode* m_pNext;
+};
+
+ListNode* CreateListNode(int value);
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext);
+void PrintListNode(ListNode* pNode);
+void PrintList(ListNode* pHead);
+void DestroyList(ListNode* pHead);
+void AddToTail(ListNode** pHead, int value);
+void RemoveNode(ListNode** pHead, int value);
+
+ListNode* CreateListNode(int value)
+{
+    ListNode* pNode = new ListNode();
+    pNode->m_nValue = value;
+    pNode->m_pNext = nullptr;
+
+    return pNode;
+}
+
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext)
+{
+    if(pCurrent == nullptr)
+    {
+        printf("Error to connect two nodes.\n");
+        exit(1);
+    }
+
+    pCurrent->m_pNext = pNext;
+}
+
+void PrintListNode(ListNode* pNode)
+{ 
+    if(pNode == nullptr)
+    {
+        printf("The node is nullptr\n");
+    }
+    else
+    {
+        printf("The key in node is %d.\n", pNode->m_nValue);
+    }
+}
+
+void PrintList(ListNode* pHead)
+{
+    printf("PrintList starts.\n");
+    
+    ListNode* pNode = pHead;
+    while(pNode != nullptr)
+    {
+        printf("%d\t", pNode->m_nValue);
+        pNode = pNode->m_pNext;
+    }
+
+    printf("\nPrintList ends.\n");
+}
+
+void DestroyList(ListNode* pHead)
+{
+    ListNode* pNode = pHead;
+    while(pNode != nullptr)
+    {
+        pHead = pHead->m_pNext;
+        delete pNode;
+        pNode = pHead;
+    }
+}
+
+void AddToTail(ListNode** pHead, int value)
+{
+    ListNode* pNew = new ListNode();
+    pNew->m_nValue = value;
+    pNew->m_pNext = nullptr;
+
+    if(*pHead == nullptr)
+    {
+        *pHead = pNew;
+    }
+    else
+    {
+        ListNode* pNode = *pHead;
+        while(pNode->m_pNext != nullptr)
+            pNode = pNode->m_pNext;
+
+        pNode->m_pNext = pNew;
+    }
+}
+
+void RemoveNode(ListNode** pHead, int value)
+{
+    if(pHead == nullptr || *pHead == nullptr)
+        return;
+
+    ListNode* pToBeDeleted = nullptr;
+    if((*pHead)->m_nValue == value)
+    {
+        pToBeDeleted = *pHead;
+        *pHead = (*pHead)->m_pNext;
+    }
+    else
+    {
+        ListNode* pNode = *pHead;
+        while(pNode->m_pNext != nullptr && pNode->m_pNext->m_nValue != value)
+            pNode = pNode->m_pNext;
+
+        if(pNode->m_pNext != nullptr && pNode->m_pNext->m_nValue == value)
+        {
+            pToBeDeleted = pNode->m_pNext;
+            pNode->m_pNext = pNode->m_pNext->m_pNext;
+        }
+    }
+
+    if(pToBeDeleted != nullptr)
+    {
+        delete pToBeDeleted;
+        pToBeDeleted = nullptr;
+    }
+}
 
 void PrintListReversingly_Iteratively(ListNode* pHead)
 {
@@ -37,6 +160,26 @@ void PrintListReversingly_Iteratively(ListNode* pHead)
     }
 }
 
+void LrPrintListReversingly_Iteratively(ListNode* pHead) {
+  std::stack<int> s;
+  ListNode* pNode = pHead;
+  while (pNode) {
+    s.push(pNode->m_nValue);
+    pNode = pNode->m_pNext;
+  }
+  while (!s.empty()) {
+    printf("%d\t", s.top());
+    s.pop();
+  }
+}
+
+void LrPrintListReversingly_Recursively(ListNode* pHead) {
+  if (pHead) {
+    LrPrintListReversingly_Recursively(pHead->m_pNext);
+    printf("%d\t", pHead->m_nValue);
+  }
+}
+
 void PrintListReversingly_Recursively(ListNode* pHead)
 {
     if(pHead != nullptr)
@@ -54,9 +197,9 @@ void PrintListReversingly_Recursively(ListNode* pHead)
 void Test(ListNode* pHead)
 {
     PrintList(pHead);
-    PrintListReversingly_Iteratively(pHead);
+    LrPrintListReversingly_Iteratively(pHead);
     printf("\n");
-    PrintListReversingly_Recursively(pHead);
+    LrPrintListReversingly_Recursively(pHead);
 }
 
 // 1->2->3->4->5
--- 07_ConstructBinaryTree/ConstructBinaryTree.cpp
+++ 07_ConstructBinaryTree/ConstructBinaryTree.cpp
@@ -18,9 +18,94 @@ https://github.com/zhedahht/CodingInterviewChinese2/blob/master/LICENSE.txt)
 // 2, 4, 7, 3, 5, 6, 8}和中序遍历序列{4, 7, 2, 1, 5, 3, 8, 6}，则重建出
 // 图2.6所示的二叉树并输出它的头结点。
 
-#include "..\Utilities\BinaryTree.h"
 #include <exception>
+#include <stdexcept>
 #include <cstdio>
+
+struct BinaryTreeNode 
+{
+    int                    m_nValue; 
+    BinaryTreeNode*        m_pLeft;  
+    BinaryTreeNode*        m_pRight; 
+};
+
+BinaryTreeNode* CreateBinaryTreeNode(int value);
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight);
+void PrintTreeNode(const BinaryTreeNode* pNode);
+void PrintTree(const BinaryTreeNode* pRoot);
+void DestroyTree(BinaryTreeNode* pRoot);
+
+BinaryTreeNode* CreateBinaryTreeNode(int value)
+{
+    BinaryTreeNode* pNode = new BinaryTreeNode();
+    pNode->m_nValue = value;
+    pNode->m_pLeft = nullptr;
+    pNode->m_pRight = nullptr;
+
+    return pNode;
+}
+
+void ConnectTreeNodes(BinaryTreeNode* pParent, BinaryTreeNode* pLeft, BinaryTreeNode* pRight)
+{
+    if(pParent != nullptr)
+    {
+        pParent->m_pLeft = pLeft;
+        pParent->m_pRight = pRight;
+    }
+}
+
+void PrintTreeNode(const BinaryTreeNode* pNode)
+{
+    if(pNode != nullptr)
+    {
+        printf("value of this node is: %d\n", pNode->m_nValue);
+
+        if(pNode->m_pLeft != nullptr)
+            printf("value of its left child is: %d.\n", pNode->m_pLeft->m_nValue);
+        else
+            printf("left child is nullptr.\n");
+
+        if(pNode->m_pRight != nullptr)
+            printf("value of its right child is: %d.\n", pNode->m_pRight->m_nValue);
+        else
+            printf("right child is nullptr.\n");
+    }
+    else
+    {
+        printf("this node is nullptr.\n");
+    }
+
+    printf("\n");
+}
+
+void PrintTree(const BinaryTreeNode* pRoot)
+{
+    PrintTreeNode(pRoot);
+
+    if(pRoot != nullptr)
+    {
+        if(pRoot->m_pLeft != nullptr)
+            PrintTree(pRoot->m_pLeft);
+
+        if(pRoot->m_pRight != nullptr)
+            PrintTree(pRoot->m_pRight);
+    }
+}
+
+void DestroyTree(BinaryTreeNode* pRoot)
+{
+    if(pRoot != nullptr)
+    {
+        BinaryTreeNode* pLeft = pRoot->m_pLeft;
+        BinaryTreeNode* pRight = pRoot->m_pRight;
+
+        delete pRoot;
+        pRoot = nullptr;
+
+        DestroyTree(pLeft);
+        DestroyTree(pRight);
+    }
+}
 
 BinaryTreeNode* ConstructCore(int* startPreorder, int* endPreorder, int* startInorder, int* endInorder);
 
@@ -49,8 +134,10 @@ BinaryTreeNode* ConstructCore
     {
         if(startInorder == endInorder && *startPreorder == *startInorder)
             return root;
-        else
-            throw std::exception("Invalid input.");
+        else {
+            std::logic_error ex("Invalid input.");
+            throw std::exception(ex);
+        }
     }
 
     // 在中序遍历中找到根结点的值
@@ -58,8 +145,10 @@ BinaryTreeNode* ConstructCore
     while(rootInorder <= endInorder && *rootInorder != rootValue)
         ++ rootInorder;
 
-    if(rootInorder == endInorder && *rootInorder != rootValue)
-        throw std::exception("Invalid input.");
+    if(rootInorder == endInorder && *rootInorder != rootValue) {
+        std::logic_error ex("Invalid input.");
+        throw std::exception(ex);
+    }
 
     int leftLength = rootInorder - startInorder;
     int* leftPreorderEnd = startPreorder + leftLength;
